91958dd7f00d7c36d79fdb20869fe93c06a2ae05
Chip Wolf ‮ <hello@chipwolf.uk>
2022-03-25 18:15:08 +0000

    chore: apply linters fixes

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,51 +1,51 @@
 $('.owl-carousel.header-wow').owlCarousel({
-	loop: true,
-	margin: 0,
-	nav: false,
-	dots: true,
-	animateOut: 'fadeOut',
-	animateIn: 'fadeIn',
-	touchDrag: false,
-	mouseDrag: false,
-	items: 1,
-	autoplay: true,
-	autoplayTimeout: 5000
-});
-var clipboard1 = new ClipboardJS('.s-copy1');
+  loop: true,
+  margin: 0,
+  nav: false,
+  dots: true,
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  touchDrag: false,
+  mouseDrag: false,
+  items: 1,
+  autoplay: true,
+  autoplayTimeout: 5000
+})
+var clipboard1 = new ClipboardJS('.s-copy1')
 clipboard1.on('success', function (e) {
-	$('.s-copy1').text('Copied ip')
-});
-var clipboard2 = new ClipboardJS('.s-copy2');
+  $('.s-copy1').text('Copied ip')
+})
+var clipboard2 = new ClipboardJS('.s-copy2')
 clipboard2.on('success', function (e) {
-	$('.s-copy2').text('copied-ip')
-});
-var clipboard3 = new ClipboardJS('.s-promo');
+  $('.s-copy2').text('copied-ip')
+})
+var clipboard3 = new ClipboardJS('.s-promo')
 clipboard3.on('success', function (e) {
-	$('.s-promo').text('Скопировано')
-});
+  $('.s-promo').text('Скопировано')
+})
 
 $('.owl-carousel.header-wow').owlCarousel({
-	loop: true,
-	margin: 0,
-	nav: false,
-	dots: true,
-	animateOut: 'fadeOut',
-	animateIn: 'fadeIn',
-	touchDrag: false,
-	mouseDrag: false,
-	items: 1,
-	autoplay: true,
-	autoplayTimeout: 5000
-});
-var clipboard1 = new ClipboardJS('.-copy1');
+  loop: true,
+  margin: 0,
+  nav: false,
+  dots: true,
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  touchDrag: false,
+  mouseDrag: false,
+  items: 1,
+  autoplay: true,
+  autoplayTimeout: 5000
+})
+var clipboard1 = new ClipboardJS('.-copy1')
 clipboard1.on('success', function (e) {
-	$('.-copy1').text('Copied ip')
-});
-var clipboard2 = new ClipboardJS('.-copy2');
+  $('.-copy1').text('Copied ip')
+})
+var clipboard2 = new ClipboardJS('.-copy2')
 clipboard2.on('success', function (e) {
-	$('.-copy2').text('copied-ip')
-});
-var clipboard3 = new ClipboardJS('.s-promo');
+  $('.-copy2').text('copied-ip')
+})
+var clipboard3 = new ClipboardJS('.s-promo')
 clipboard3.on('success', function (e) {
-	$('.s-promo').text('Скопировано')
-});
+  $('.s-promo').text('Скопировано')
+})
--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,159 +1,160 @@
-const item1 = document.querySelector("#carousel-item-1");
-const item2 = document.querySelector("#carousel-item-2");
-const item3 = document.querySelector("#carousel-item-3");
-const item4 = document.querySelector("#carousel-item-4");
+const item1 = document.querySelector('#carousel-item-1')
+const item2 = document.querySelector('#carousel-item-2')
+const item3 = document.querySelector('#carousel-item-3')
+const item4 = document.querySelector('#carousel-item-4')
 
-const header = document.querySelector("header");
+const header = document.querySelector('header')
 
-let currentItem = item1;
-let sliderCooldown = false;
+let currentItem = item1
+let sliderCooldown = false
 
-const button1 = document.querySelectorAll(".carousel-button-1");
-const button2 = document.querySelectorAll(".carousel-button-2");
-const button3 = document.querySelectorAll(".carousel-button-3");
-const button4 = document.querySelectorAll(".carousel-button-4");
+const button1 = document.querySelectorAll('.carousel-button-1')
+const button2 = document.querySelectorAll('.carousel-button-2')
+const button3 = document.querySelectorAll('.carousel-button-3')
+const button4 = document.querySelectorAll('.carousel-button-4')
 
-button1.forEach(function(button) {
-    button.addEventListener("click", function() {
-        if (currentItem !== item1) {
-            currentItem.classList.add("animation-fadeout");
-            currentItem = item1;
+button1.forEach(function (button) {
+  button.addEventListener('click', function () {
+    if (currentItem !== item1) {
+      currentItem.classList.add('animation-fadeout')
+      currentItem = item1
 
-            //header.style.background = "url(media/download-bg.png) no-repeat bottom center";
-            //header.style.backgroundSize = "cover";
+      // header.style.background = "url(media/download-bg.png) no-repeat bottom center";
+      // header.style.backgroundSize = "cover";
 
-            startCooldown();
-        }
-    });
-});
-
-item1.addEventListener("animationend", (event) => {
-    if (event.type === "animationend") {
-        if (event.animationName === "fadeout") {
-            item1.classList.add("hidden");
-            item1.classList.remove("animation-fadeout");
-
-            currentItem.classList.remove("hidden");
-            currentItem.classList.add("animation-fadein");
-        } else if (event.animationName === "fadein") {
-            item1.classList.remove("animation-fadein");
-        }
+      startCooldown()
     }
-});
+  })
+})
 
-button2.forEach(function(button) {
-    button.addEventListener("click", function() {
-        if (currentItem !== item2) {
-            currentItem.classList.add("animation-fadeout");
-            currentItem = item2;
+item1.addEventListener('animationend', (event) => {
+  if (event.type === 'animationend') {
+    if (event.animationName === 'fadeout') {
+      item1.classList.add('hidden')
+      item1.classList.remove('animation-fadeout')
 
-            //header.style.background = "url(media/bg-head-2.png) no-repeat bottom center";
-            //header.style.backgroundSize = "cover";
-
-            startCooldown();
-        }
-    });
-});
-
-item2.addEventListener("animationend", (event) => {
-    if (event.type === "animationend") {
-        if (event.animationName === "fadeout") {
-            item2.classList.add("hidden");
-            item2.classList.remove("animation-fadeout");
-
-            currentItem.classList.remove("hidden");
-            currentItem.classList.add("animation-fadein");
-        } else if (event.animationName === "fadein") {
-            item2.classList.remove("animation-fadein");
-        }
+      currentItem.classList.remove('hidden')
+      currentItem.classList.add('animation-fadein')
+    } else if (event.animationName === 'fadein') {
+      item1.classList.remove('animation-fadein')
     }
-});
+  }
+})
 
-button3.forEach(function(button) {
-    button.addEventListener("click", function() {
-        if (currentItem !== item3) {
-            currentItem.classList.add("animation-fadeout");
-            currentItem = item3;
+button2.forEach(function (button) {
+  button.addEventListener('click', function () {
+    if (currentItem !== item2) {
+      currentItem.classList.add('animation-fadeout')
+      currentItem = item2
 
-            //header.style.background = "url(media/bg-head-3.png) no-repeat bottom center";
-            //header.style.backgroundSize = "cover";
+      // header.style.background = "url(media/bg-head-2.png) no-repeat bottom center";
+      // header.style.backgroundSize = "cover";
 
-            startCooldown();
-        }
-    });
-});
-
-item3.addEventListener("animationend", (event) => {
-    if (event.type === "animationend") {
-        if (event.animationName === "fadeout") {
-            item3.classList.add("hidden");
-            item3.classList.remove("animation-fadeout");
-
-            currentItem.classList.remove("hidden");
-            currentItem.classList.add("animation-fadein");
-        } else if (event.animationName === "fadein") {
-            item3.classList.remove("animation-fadein");
-        }
+      startCooldown()
     }
-});
+  })
+})
 
-button4.forEach(function(button) {
-    button.addEventListener("click", function() {
-        if (currentItem !== item4) {
-            currentItem.classList.add("animation-fadeout");
-            currentItem = item4;
+item2.addEventListener('animationend', (event) => {
+  if (event.type === 'animationend') {
+    if (event.animationName === 'fadeout') {
+      item2.classList.add('hidden')
+      item2.classList.remove('animation-fadeout')
 
-            //header.style.background = "url(media/bg-head-4.png) no-repeat bottom center";
-            //header.style.backgroundSize = "cover";
-
-            startCooldown();
-        }
-    });
-});
-
-item4.addEventListener("animationend", (event) => {
-    if (event.type === "animationend") {
-        if (event.animationName === "fadeout") {
-            item4.classList.add("hidden");
-            item4.classList.remove("animation-fadeout");
-
-            currentItem.classList.remove("hidden");
-            currentItem.classList.add("animation-fadein");
-        } else if (event.animationName === "fadein") {
-            item4.classList.remove("animation-fadein");
-        }
+      currentItem.classList.remove('hidden')
+      currentItem.classList.add('animation-fadein')
+    } else if (event.animationName === 'fadein') {
+      item2.classList.remove('animation-fadein')
     }
-});
+  }
+})
+
+button3.forEach(function (button) {
+  button.addEventListener('click', function () {
+    if (currentItem !== item3) {
+      currentItem.classList.add('animation-fadeout')
+      currentItem = item3
+
+      // header.style.background = "url(media/bg-head-3.png) no-repeat bottom center";
+      // header.style.backgroundSize = "cover";
+
+      startCooldown()
+    }
+  })
+})
+
+item3.addEventListener('animationend', (event) => {
+  if (event.type === 'animationend') {
+    if (event.animationName === 'fadeout') {
+      item3.classList.add('hidden')
+      item3.classList.remove('animation-fadeout')
+
+      currentItem.classList.remove('hidden')
+      currentItem.classList.add('animation-fadein')
+    } else if (event.animationName === 'fadein') {
+      item3.classList.remove('animation-fadein')
+    }
+  }
+})
+
+button4.forEach(function (button) {
+  button.addEventListener('click', function () {
+    if (currentItem !== item4) {
+      currentItem.classList.add('animation-fadeout')
+      currentItem = item4
+
+      // header.style.background = "url(media/bg-head-4.png) no-repeat bottom center";
+      // header.style.backgroundSize = "cover";
+
+      startCooldown()
+    }
+  })
+})
+
+item4.addEventListener('animationend', (event) => {
+  if (event.type === 'animationend') {
+    if (event.animationName === 'fadeout') {
+      item4.classList.add('hidden')
+      item4.classList.remove('animation-fadeout')
+
+      currentItem.classList.remove('hidden')
+      currentItem.classList.add('animation-fadein')
+    } else if (event.animationName === 'fadein') {
+      item4.classList.remove('animation-fadein')
+    }
+  }
+})
 
 // Automatically go to next page in the carousel
-//setInterval(autoSlide, 12000);
+// setInterval(autoSlide, 12000);
 
-function autoSlide() {
-    if (!sliderCooldown)
-        switch (currentItem) {
-            case item1:
-                button2[0].click();
-                break;
-            case item2:
-                button3[1].click();
-                break;
-            case item3:
-                button4[2].click();
-                break;
-            case item4:
-                button1[3].click();
-                break;
-        }
+function autoSlide () {
+  if (!sliderCooldown) {
+    switch (currentItem) {
+      case item1:
+        button2[0].click()
+        break
+      case item2:
+        button3[1].click()
+        break
+      case item3:
+        button4[2].click()
+        break
+      case item4:
+        button1[3].click()
+        break
+    }
+  }
 }
 
 // Slider Cooldown
-function startCooldown() {
-    if (!sliderCooldown) {
-        sliderCooldown = true;
-        setInterval(endCooldown, 12000);
-    }
+function startCooldown () {
+  if (!sliderCooldown) {
+    sliderCooldown = true
+    setInterval(endCooldown, 12000)
+  }
 }
 
-function endCooldown() {
-    sliderCooldown = false;
+function endCooldown () {
+  sliderCooldown = false
 }
